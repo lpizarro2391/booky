a30e0795be746f84b85ed6569458d9c6187919fd
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2021-04-12 16:47:28 -0600

    8. flash() es una funcion para mostrar mensajes parpadeantes que revisan si se cambio o no el nombre, entonces en hello.py se debe de agregar las extensiones necesarias y se debe modifiacr el app.route de index para que reconozca el viejo nombre y el nuvo nombre, ademas para que se renderize debe ise a base .html a poner la mascarilla de tal forma que se muestra en todas las paginas que se usen de la misma forma.

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, render_template,session, redirect, url_for
+from flask import Flask, render_template,session, redirect, url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -21,11 +21,14 @@ moment = Moment(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm() 
-    if form.validate_on_submit(): 
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session ['name'] = form.name.data 
         return redirect(url_for('index'))
     return render_template('index.html', form=form, 
-    name=session.get('name'), current_time=datetime.utcnow())
+    name = session.get('name'),current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
