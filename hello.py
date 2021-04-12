eb0975f5337df6d7a09d006bc8cdfbfdf34c7fb9
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2021-04-12 14:29:55 -0600

    5. class variables, each class variable is assigned an object associated with the field type.  StringField class represesnets and HTML<input> element with a type = "submit" atribute.

--- hello.py
+++ hello.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
