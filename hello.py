992ab2effed972d05e13b4094de3718ad044db9f
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2021-04-12 16:23:24 -0600

    7. con este cambo en index, se le indica al navegador que no muestre el mensaje de refrescar que dice que si esta seguro que desea regrescar, pero es porque el asume que cuando se refresaca es una instruccion POST , entonces con eso se redirecciona a la session de usuario y ya no muestra el mensaje

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template,session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -18,12 +19,13 @@ moment = Moment(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index(): 
-    name = None 
+def index():
     form = NameForm() 
-    if form.validate_on_submit(): name = form.name.data 
-    form.name.data = '' 
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+    if form.validate_on_submit(): 
+        session ['name'] = form.name.data 
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, 
+    name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
