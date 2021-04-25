b87cb00513d1ff47519caff550e0e387fa668c69
lpizarro <lpizarro2391@github.com>
2021-04-24 19:30:09 -0600

    13. In addition to the MAIL_USERNAME and MAIL_PASSWORD environment variables described earlier, this version of the application needs the FLASKY_ADMIN environment variable. For Linux and macOS users, the command to set this variable is:

--- hello.py
+++ hello.py
@@ -7,10 +7,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 #to  create database#
-import os
 from flask_sqlalchemy import SQLAlchemy
 #to update de basadate#
 from flask_migrate import Migrate
+#Flask-Mail is initialized as shown #
+from flask_mail import Mail
+import os
+#this function can render email bodies from Jinja2 templates to have the most flexibility.#
+from flask_mail import Message
+
+
+
 
 
 
@@ -35,6 +42,22 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 # to migrate# 
 migrate = Migrate(app, db)
+#Flask-Mail is initialized as shown #
+mail = Mail(app)
+#config de email, to send email from gmail#
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+#Integrating Emails with the Application#
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]' 
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin mypleasurehelpyou@gmail.com'
+# email example#
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
+
 
 #configuration of database Role and User model definition#
 
@@ -72,6 +95,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],
+                           'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
             session['name'] = form.name.data
@@ -101,5 +127,11 @@ def internal_server_error(e):
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-
-
+#Integrating Emails with the Application#
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=
+    [to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
